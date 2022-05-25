b627a4b6cd523b5bf73d2cbebcc5c795a3ae2a5b
karurochari <nope>
2022-05-25 17:57:57 +0000

    Maybe fixed relative path

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import {dirname} from 'path';
 import initModels from "./db-schemas/init-models.js"
 import { Sequelize, Op, Model, DataTypes } from 'sequelize';
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 let API = []
 
-let files = await fs.readdir('./api-schemas/');
+let files = await fs.readdir(__dirname+'/api-schemas/');
 
 for(const f in files) {
-	API[files[f]] = JSON.parse(await fs.readFile(`./api-schemas/${files[f]}`));
+	API[files[f]] = JSON.parse(await fs.readFile(`${__dirname}/api-schemas/${files[f]}`));
 }
 
-console.log(API)
+//console.log(API)
 
 export const tausCommon = {
 	apiSchemas: API,
